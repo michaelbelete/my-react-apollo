dc9f7fd9fdbb2a529afae9f115663e1dc90631af
Michael Belete <it.michael.belete@gmail.com>
2020-08-05 17:26:09 +0300

    added flag to list

--- src/Countries.js
+++ src/Countries.js
@@ -1,25 +1,40 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
-
+import { ListGroup, ListGroupItem, Row, Col } from "react-bootstrap";
 export default function Countries() {
   const FETCH_COUNTRIES = gql`
     {
       Country {
         name
-        capital
-        area
-        population
+        flag {
+          svgFile
+        }
       }
     }
   `;
   const { loading, error, data } = useQuery(FETCH_COUNTRIES);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p> Error: {error} :( </p>;
+  if (loading) return <p className="text-center">Loading...</p>;
+  if (error) return <p className="text-center"> Error: {error} :( </p>;
   return data.Country.map( (country) => (
-    <div key={country.name}>
-      <li>
-        {country.name} : {country.capital}
-      </li>
+    <div className="pt-3" key={country.name}>
+      <Row>
+        <Col></Col>
+        <Col>
+          <ListGroup>
+            <ListGroup.Item> 
+              <img
+                width={25}
+                height={25}
+                className="mr-3"
+                src= { country.flag.svgFile }
+                alt="Generic placeholder"
+              />
+              {country.name}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col></Col>
+      </Row>
     </div>
   ));
 }
