931e7b5da111e4888987b521bbe019c6fcaabb2c
Michael Belete <it.michael.belete@gmail.com>
2020-08-06 20:15:06 +0300

    working on pagination limited number of outputs to 6 only fetch more left

--- src/Countries.js
+++ src/Countries.js
@@ -3,8 +3,8 @@ import { useQuery, gql } from "@apollo/client";
 import { ListGroup, ListGroupItem, Row, Col } from "react-bootstrap";
 export default function Countries() {
   const FETCH_COUNTRIES = gql`
-    {
-      Country {
+    query Country($offset: Int, $limit: Int){
+      Country(offset: $offset, first: $limit){
         name
         flag {
           svgFile
@@ -12,9 +12,20 @@ export default function Countries() {
       }
     }
   `;
-  const { loading, error, data } = useQuery(FETCH_COUNTRIES, { pollInterval: 500, });
+  const { loading, error, data, fetchMore } = useQuery(FETCH_COUNTRIES, 
+    { 
+      variables: {
+        offset: 0,
+        limit: 6
+      },
+      fetchPolicy: "cache-and-network",
+      pollInterval: 500,
+    }
+  );
   if (loading) return <p className="text-center">Loading...</p>;
+
   if (error) return <p className="text-center"> Error: {error} :( </p>;
+    
   return data.Country.map( (country) => (
     <div className="pt-3" key={country.name}>
       <Row>
@@ -36,5 +47,7 @@ export default function Countries() {
         <Col></Col>
       </Row>
     </div>
+    
   ));
+
 }
